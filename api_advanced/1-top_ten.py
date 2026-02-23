a2daffb18a8e8c83737f8c13996a100839b7f659
josephmarube <j.marube@alustudent.com>
2026-02-23 19:09:35 +0200

    task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first
-10 hot posts for a given subreddit.
+Module that queries the Reddit API and prints
+the titles of the first 10 hot posts.
 """
-
 import requests
 
 
 def top_ten(subreddit):
-    """Print top 10 hot post titles."""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """
+    Prints the top 10 hot post titles of a subreddit.
+    If invalid subreddit, prints None.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "alu-api-advanced"}
 
-    headers = {
-        "User-Agent": "linux:api.advanced.project:v1.0 (by /u/anonymous)"
-    }
-
-    params = {
-        "limit": 10
-    }
-
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        print(None)
+        print("None")
         return
 
-    data = response.json().get("data", {})
-    if not data:
-        print(None)
-        return
-    
-    posts = data.get("children", [])
+    posts = response.json().get("data", {}).get("children", [])
 
     for post in posts:
         print(post.get("data", {}).get("title"))

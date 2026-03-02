3a409f7c5454b8d848a7851a00da4817e1694e48
Joseph Nyabando Marube <j.marube@alustudent.com>
2026-03-02 11:55:29 +0200

    Update User-Agent header for Reddit API requests

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "linux:my.reddit.app:v1.0 (by /u/test)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
